c03f3e24e227c48b31d3fd744a74ecbe99ee169e
mahmoudMoustafaFawzy <mahmoudmoustafafawzy>
2024-07-14 20:04:20 +0300

    first commit

---
+++ Exam2/js/allFuc.js
@@ -0,0 +1,22 @@
+
+
+ export default function ay7ga()
+{
+    export function test()
+{
+    console.log("Hiii tests")
+}
+
+
+export default function demo()
+{
+    console.log("Hiii demo")
+}
+
+export function Welcome()
+{
+    console.log("Hiii welcome")
+}
+
+
+}
